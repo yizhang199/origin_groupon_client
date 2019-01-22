0b5a513a139d91042f575af7a7284340d3e6540b
yizhang19801001 <yizhang19801001@gmail.com>
2019-01-22 17:50:42 +1100

    style order item card

--- src/components/OrderItemCard.jsx
+++ src/components/OrderItemCard.jsx
@@ -12,10 +12,49 @@ class OrderItemCard extends React.Component {
   decrease = () => {
     this.props.decreaseOrderItem(this.props.orderItem);
   };
+
+  renderButtonGroup = () => {
+    if (this.props.orderItem.quantity > 0) {
+      return (
+        <div className="component-order-item-card__button-group">
+          <i onClick={this.decrease} className="material-icons">
+            remove_circle
+          </i>
+          <span className="component-order-item-card__product-quantity">
+            {this.props.orderItem.quantity}
+          </span>
+          <i onClick={this.increase} className="material-icons">
+            add_circle
+          </i>
+        </div>
+      );
+    } else {
+      return (
+        <div className="component-order-item-card__button-group-init">
+          <i onClick={this.increase} className="material-icons">
+            add_circle
+          </i>
+        </div>
+      );
+    }
+  };
   render() {
     return (
       <div className="component-order-item-card">
-        {this.props.orderItem.item.name} x {this.props.orderItem.quantity}
+        <div className="component-order-item-card__left">
+          <span className="component-order-item-card__name">
+            {this.props.orderItem.item.name}
+          </span>{" "}
+        </div>
+        <div className="component-order-item-card__middle">
+          <span>sample options</span>
+        </div>
+        <div className="component-order-item-card__right">
+          <span className="component-order-item-card__price">
+            ${this.props.orderItem.item.price}
+          </span>
+          {this.renderButtonGroup()}
+        </div>
       </div>
     );
   }

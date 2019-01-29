b7b2fe96d36e18c1d895d21de8476ebe5d319bbb
yi zhang <yizhang19801001@outlook.com>
2019-01-30 08:04:48 +1100

    shopping cart icon added

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -11,9 +11,52 @@ class ShoppingCart extends React.Component {
 
     this.state = { showList: false };
   }
-  renderIcon = () => {
-    return <div />;
+
+  /**
+   * calculate total quantity of shopping cart list
+   * @returns {Integer} total quantity of shopping cart list
+   */
+  getTotal = () => {
+    let count = 0;
+    this.props.shoppingCartList.map(orderItem => {
+      count += orderItem.quantity;
+    });
+
+    return count;
   };
+
+  /**
+   * render quantity badge for shopping cart icon
+   * @returns {JSX} badge of quantity
+   */
+  renderQuantityBadge = () => {
+    const count = this.getTotal();
+    if (count > 0) {
+      return (
+        <span className="component-shopping-cart__icon-quantity">{count}</span>
+      );
+    }
+    return null;
+  };
+
+  /**
+   * render jsx for shopping cart icon
+   * @returns {JSX} icon of shopping cart and quantity badge
+   */
+  renderIcon = () => {
+    return (
+      <div className="component-shopping-cart__icon-container">
+        <i className="material-icons">shopping_cart</i>
+
+        {this.renderQuantityBadge()}
+      </div>
+    );
+  };
+
+  /**
+   * render jsx for shopping cart list
+   * @returns {JSX} list of OrderItemCard
+   */
   renderList = () => {
     if (this.state.showList && this.props.shoppingCartList.length > 0) {
       return (
@@ -31,8 +74,16 @@ class ShoppingCart extends React.Component {
       return null;
     }
   };
+
+  /**
+   * control show / hide shopping cart list
+   * @param {void}
+   * @returns {void}
+   */
   toggleList = () => {
-    this.setState({ showList: !this.state.showList });
+    if (this.props.shoppingCartList.length > 0) {
+      this.setState({ showList: !this.state.showList });
+    }
   };
   render() {
     return (

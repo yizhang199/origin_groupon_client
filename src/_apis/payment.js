974b528f0fac101a79496be445d0ea3203a12b23
yi zhang <yizhang19801001@outlook.com>
2019-03-11 08:05:09 +1100

    etc

--- src/_apis/payment.js
+++ src/_apis/payment.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://kidsnparty.com.au/redpay/public/api/payments/`
+  // baseURL: `http://kidsnparty.com.au/redpay/public/api/payments/`
+  baseURL: `http://localhost:8000/api/payment/`
 });

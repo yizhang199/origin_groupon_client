54e569b964f9514fe1450d8a4d2eb1e58c433b8c
yi zhang <yizhang19801001@outlook.com>
2019-03-10 11:40:02 +1100

    fetch products according by user_group_id

--- src/_actions/Product.js
+++ src/_actions/Product.js
@@ -1,13 +1,15 @@
 import types from "./actionTypes";
 import { kidsnParty } from "../_apis/";
+import { makeHeader } from "../_helpers";
 
 const index = () => {
   const language_id = localStorage.getItem("aupos_language_id")
     ? localStorage.getItem("aupos_language_id")
     : null;
-
+  const headers = makeHeader();
   return async function(dispatch) {
     const response = await kidsnParty.get(`/products`, {
+      headers,
       params: { language_id }
     });
 

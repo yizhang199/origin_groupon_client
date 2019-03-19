fd507ea2d1e3df4e5ea94c06922a095ec40fd507
yizhang19801001 <yizhang19801001@gmail.com>
2019-03-19 13:45:28 +1100

    order list short mode

--- src/app/Account/OrderCard.jsx
+++ src/app/Account/OrderCard.jsx
@@ -11,127 +11,132 @@ import { makeDate } from "../../_helpers";
  * @param {React.Props} props
  * @returns {JSX} order card component
  */
-const OrderCard = props => {
+class OrderCard extends React.Component {
+  state = { isShowDetail: false };
   /**
    * render list of order items for certain order
    * @param {Array} list
-   * @returns {JSX}
+   * @returns {String}
    */
-  const renderOrderList = list => {
-    return (
-      <div className="component-order-card__body__list">
-        {list.map((orderItem, index) => {
-          return (
-            <span
-              key={`orderItem${index}`}
-              className="component-order-card__body__list-item"
-            >
-              <span>{orderItem.name}</span> x <span>{orderItem.quantity},</span>
-            </span>
-          );
-        })}
-      </div>
-    );
+  renderOrderList = list => {
+    let summary = "";
+    list.map(orderItem => {
+      summary += `${orderItem.name} x ${orderItem.quantity}, `;
+    });
+    return summary;
   };
-
   /**
-   * render JSX for button group or text information depending order status
-   * @param {Object<Order>} order
-   * @returns {JSX}
+   * get order total quantity of items
+   * @param {Array} list
+   * @returns {Integer} total_quantity
    */
-  const renderOrderCardFooter = order => {
-    if (parseInt(order.status_id) === 1) {
-      return (
-        <div className="component-order-card__footer">
-          <button
-            className="component-order-card__footer__button-remove"
-            onClick={deleteOrder}
-          >
-            删除订单
-          </button>
-          <button
-            className="component-order-card__footer__button-continue"
-            onClick={continueOrder}
-          >
-            继续点单
-          </button>
-        </div>
-      );
-    } else {
-      return (
-        <div className="component-order-card__footer">
-          <div className="component-order-card__footer__intro">
-            该订单已经生成，若需修改请联系客服。
-          </div>
-        </div>
-      );
+  getTotalQuantity = list => {
+    let count = 0;
+    list.map(orderItem => {
+      count += orderItem.quantity;
+    });
+
+    return count;
+  };
+  /**
+   * get order status in text name
+   * @param {Integer} order_status_id
+   * @returns {JSX} name of status
+   */
+  getStatusName = id => {
+    switch (parseInt(id)) {
+      case 2:
+        return <span className="status pending">{`待取货`}</span>;
+      case 1:
+        return (
+          <span
+            className="status saved"
+            onClick={this.continueOrder}
+          >{`继续>>`}</span>
+        );
+      case 3:
+        return <span className="status complete">{`已完成`}</span>;
+      default:
+        break;
     }
   };
+
   /**
    * call action delete order by calling api
    * @param {void}
    * @returns {void}
    */
 
-  const deleteOrder = () => {
-    props.deleteOrder(props.order);
+  deleteOrder = () => {
+    this.props.deleteOrder(this.props.order);
   };
   /**
    * call aciont function to change state in redux store
    * @param {void}
    * @returns {Void} call action function
    */
-  const continueOrder = () => {
-    props.renderNewShoppingCart(props.order);
+  continueOrder = () => {
+    this.props.renderNewShoppingCart(this.props.order);
   };
-  const order = props.order;
-  return (
-    <div className="component-order-card">
-      <div className="component-order-card__header">
-        <span className="component-order-card__header__left">
-          <span className="component-order-card__header__invoice-no">
-            <span className="component-order-card__header__invoice-no__title">
-              订单号
-            </span>
-            <span className="component-order-card__header__invoice-no__value">
-              {order.invoice_no}
-            </span>
+
+  render() {
+    const order = this.props.order;
+    return (
+      <div className="component-order-card">
+        <div
+          className="header"
+          onClick={() =>
+            this.setState({ isShowDetail: !this.state.isShowDetail })
+          }
+        >
+          <span className="information-row">
+            <span className="title">订单号:</span>
+            <span className="value">{order.invoice_no}</span>
           </span>
-          <span className="component-order-card__header__added-date">
-            <span className="component-order-card__header__added-date__title">
-              创建日期
-            </span>
-            <span className="component-order-card__header__added-date__value">
-              {makeDate(order.create_date)}
-            </span>
+          <span className="toggle">
+            <i className="material-icons">
+              {this.state.isShowDetail
+                ? `keyboard_arrow_down`
+                : `keyboard_arrow_up`}
+            </i>
           </span>
-        </span>
-        <span className="component-order-card__header__right">
-          <span className="component-order-card__header__picked-shop">
-            <span className="component-order-card__header__picked-shop__title">
-              取货地点
+        </div>
+        <div className="card-body">
+          <div className="information">
+            <span className="information-row">
+              <span className="title">创建日期: </span>
+              <span className="value"> {makeDate(order.create_date)}</span>
             </span>
-            <span className="component-order-card__header__picked-shop__value">
-              {order.store_name}
+            <span className="information-row">
+              <span className="title">取货地点: </span>
+              <span className="value"> {order.store_name}</span>
             </span>
-          </span>
-          <span className="component-order-card__header__picked-date">
-            <span className="component-order-card__header__picked-date__title">
-              取货时间
+            <span className="information-row">
+              <span className="title">取货时间: </span>
+              <span className="value"> {makeDate(order.picked_date)}</span>
             </span>
-            <span className="component-order-card__header__picked-date__value">
-              {makeDate(order.picked_date)}
+            <span className="information-row">
+              <span className="title">产品明细: </span>
+              <span className="value">
+                {this.renderOrderList(order.order_items)}
+              </span>
             </span>
-          </span>
-        </span>
+          </div>
+          <div className="quantity-status">
+            {this.getStatusName(order.status_id)}
+
+            <span className="quantity">
+              <span className="value">
+                {this.getTotalQuantity(order.order_items)}
+              </span>
+              <span className="unit">{`items`}</span>
+            </span>
+          </div>
+        </div>
       </div>
-      <div className="component-order-card__body">
-        {renderOrderList(order.order_items)}
-      </div>
-      {renderOrderCardFooter(order)}
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default connect(
   null,
